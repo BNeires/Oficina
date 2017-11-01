0176147c0f38d17106b027c03dae393323373e60
bneir <bneir@10.50.23.42>
2017-11-01 11:14:36 -0400

    scacascsacs

--- Oficina/src/cadastroFuncionario.java
+++ Oficina/src/cadastroFuncionario.java
@@ -1,4 +1,13 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class cadastroFuncionario {
+import javax.swing.JFrame;
+
+public class cadastroFuncionario extends JFrame implements ActionListener {
+
+	
+	public void actionPerformed(ActionEvent arg0) {
+		
+	}
 
 }
