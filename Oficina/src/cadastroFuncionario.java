2396fdd9fe30feb2764f85ffe66a44602c69fd50
bneir <bneir@10.50.23.42>
2017-11-01 11:23:12 -0400

    Vai cagar

--- Oficina/src/cadastroFuncionario.java
+++ Oficina/src/cadastroFuncionario.java
@@ -5,6 +5,9 @@ import javax.swing.JFrame;
 
 public class cadastroFuncionario extends JFrame implements ActionListener {
 
+	private String url = "jdbc:mysql://127.0.0.1:3306/aula1110?useSSL=false",
+			usuario = "root",
+			senha = "85002062";
 	
 	public void actionPerformed(ActionEvent arg0) {
 		
